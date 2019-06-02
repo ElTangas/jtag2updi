ac1941c9a09c9621e8e53d1d273fb0247eb6fc88
ElTangas <35045048+ElTangas@users.noreply.github.com>
2019-06-02 23:15:00 +0100

    Compatibility fixes

--- source/sys.h
+++ source/sys.h
@@ -14,8 +14,70 @@
 #define PORT(x) CONCAT(PORT,x)
 #define DDR(x) CONCAT(DDR,x)
 
-#include <avr/pgmspace.h>
+// Configuration for Arduino Mega
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+# ifndef UPDI_PORT
+#  define UPDI_PORT D
+# endif
 
+# ifndef UPDI_PIN
+#  define UPDI_PIN 3
+# endif
+
+# ifndef LED_PORT
+#  define LED_PORT B
+# endif
+
+# ifndef LED_PIN
+#  define LED_PIN 7
+# endif
+
+# ifndef UPDI_IO_TYPE
+#  define UPDI_IO_TYPE 2
+# endif
+#endif
+
+
+// Default configuration (suitable for ATmega 328P and similar devices @16MHz)
+#ifndef UPDI_PORT
+#define UPDI_PORT D
+#endif
+
+#ifndef UPDI_PIN
+#define UPDI_PIN 6
+#endif
+
+#ifndef LED_PORT
+#define LED_PORT B
+#endif
+
+#ifndef LED_PIN
+#define LED_PIN 5
+#endif
+
+#ifndef F_CPU
+#define F_CPU 16000000U
+#endif
+
+#ifndef UPDI_BAUD
+#define UPDI_BAUD 225000U	// (max 225000 min approx. F_CPU/100)
+#endif
+
+/*
+ * Available UPDI I/O types are:
+ *
+ * 1 - timer sofware UART:		Compatible only with Mega328P and other AVRs with identical 8 bit timer 0.
+ *								Only the OC0A pin can be used for UPDI I/O. Slightly faster upload speed for a given UPDI_BAUD value.
+ *
+ * 2 - bitbang software UART:	Compatible with many chips and broad choice of UPDI pins are selectable.
+ *								Slightly slower upload speed for a given UPDI_BAUD value. Download speed is the same.
+ */
+#ifndef UPDI_IO_TYPE
+#define UPDI_IO_TYPE 1		
+#endif
+
+// Flash constants class
+#include <avr/pgmspace.h>
 #define FLASH const PROGMEM flash
 
 template <typename T>
