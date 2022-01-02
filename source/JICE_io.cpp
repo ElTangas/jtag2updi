b5e6c08da35946fb12a7633e9ae406789e6b98cb
MCUdude <hansibull@gmail.com>
2022-01-02 19:30:56 +0100

    replace semicolon with comma Can't believe how this slipped though...

--- source/JICE_io.cpp
+++ source/JICE_io.cpp
@@ -24,28 +24,28 @@ namespace {
     baud_reg_val(115200),
     baud_reg_val(14400),
     // Extension to jtagmkII protocol: extra baud rates, standard series.
-    baud_reg_val(153600);
-    baud_reg_val(230400);
-    baud_reg_val(460800);
-    baud_reg_val(921600);
+    baud_reg_val(153600),
+    baud_reg_val(230400),
+    baud_reg_val(460800),
+    baud_reg_val(921600),
     // Extension to jtagmkII protocol: extra baud rates, binary series.
-    baud_reg_val(128000);
-    baud_reg_val(256000);
-    baud_reg_val(512000);
-    baud_reg_val(1024000);
+    baud_reg_val(128000),
+    baud_reg_val(256000),
+    baud_reg_val(512000),
+    baud_reg_val(1024000),
     // Extension to jtagmkII protocol: extra baud rates, decimal series.
-    baud_reg_val(150000);
-    baud_reg_val(200000);
-    baud_reg_val(250000);
-    baud_reg_val(300000);
-    baud_reg_val(400000);
-    baud_reg_val(500000);
-    baud_reg_val(600000);
-    baud_reg_val(666666);
-    baud_reg_val(1000000);
-    baud_reg_val(1500000);
-    baud_reg_val(2000000);
-    baud_reg_val(3000000);
+    baud_reg_val(150000),
+    baud_reg_val(200000),
+    baud_reg_val(250000),
+    baud_reg_val(300000),
+    baud_reg_val(400000),
+    baud_reg_val(500000),
+    baud_reg_val(600000),
+    baud_reg_val(666666),
+    baud_reg_val(1000000),
+    baud_reg_val(1500000),
+    baud_reg_val(2000000),
+    baud_reg_val(3000000)
     };
 }
 
